4d644e568bf8ea3e0e5505ca9877d3cfcb12de0b
Amit Kumar <37330833+Amittyagivats@users.noreply.github.com>
2024-03-17 14:00:58 +0530

    Delete resources directory

--- resources/elements.py
+++
@@ -1,5 +0,0 @@
-
-class PageElements:
-    
-    SEARCH_FIELD = "(//input[@aria-label='Search for stocks, ETFs & more'])[2]"
-
--- resources/locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-from resources.elements import PageElements
-
-
-class PageLocators():
-    SEARCH_STOCKS = (By.XPATH, PageElements.SEARCH_FIELD)
-    
